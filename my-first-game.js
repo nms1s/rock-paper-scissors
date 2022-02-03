f1e85c6a6be937a880707c197e3f7e451a990c8f
nms1s <sailbeyondthedusk@gmail.com>
2022-02-02 20:01:08 -0500

    added graphics and sounds

--- my-first-game.js
+++ my-first-game.js
@@ -8,54 +8,53 @@ function computerPlay(){
     computerSelection = computerChoices[Math.floor(Math.random()*computerChoices.length)];
 }
 
-computerPlay(); // give computerSelection an initial value.
+
 
 
 function playRound(){
     if (computerSelection === playerSelection) {
         narratorText.innerText = "That's a Tie.";
-        computerPlay();
+        tieRoundAudio.play();
         checkWinner();
     }
     else if (computerSelection === "rock" && playerSelection === "scissors"){
         narratorText.innerText = "You lost.\nRock beats scissors.\nLets try again.";
-        ++computerPoints;
-        computerPlay();  
+        lostRoundAudio.play();
+        ++computerPoints;  
         checkWinner();
     }
     else if (computerSelection === "rock" && playerSelection === "paper"){
         narratorText.innerText = "Easy win!\nPaper beats rock.\nLet's go again!";
-        ++playerPoints;
-        computerPlay();  
+        wonRoundAudio.play();
+        ++playerPoints;  
         checkWinner();
     }
     else if (computerSelection === "paper" && playerSelection === "scissors"){
         narratorText.innerText = "Your win!\nScissors beat paper.";
-        ++playerPoints;
-        computerPlay();  
+        wonRoundAudio.play();
+        ++playerPoints;  
         checkWinner();
     }
     else if (computerSelection === "paper" && playerSelection === "rock"){
         narratorText.innerText = "Ouch, that's a loss.\n Paper beats rock\nGive it another try.";
+        lostRoundAudio.play();
         ++computerPoints;
-        computerPlay();
         checkWinner();  
     }
     else if (computerSelection === "scissors" && playerSelection === "paper"){
         narratorText.innerText = "You lose...\nScissors beat paper\nLet's go again.";
+        lostRoundAudio.play();
         ++computerPoints;
-        computerPlay();
         checkWinner();  
     }
     else if (computerSelection === "scissors" && playerSelection === "rock"){
         narratorText.innerText = "SMASH! Rock beats scissors.\nWell done!";
-        ++playerPoints;
-        computerPlay(); 
+        wonRoundAudio.play();
+        ++playerPoints; 
         checkWinner(); 
     }
     else {
         narratorText.innerText = "Hmmm something went wrong.";
-        computerPlay();
         checkWinner();
     }
     
@@ -67,16 +66,19 @@ function checkWinner(){
         declareWinner();
         playerPoints = 0;
         computerPoints = 0;
-        //startNewGame();
     }  
 }
 
 function declareWinner(){
-    narratorText.innerText = playerPoints === 5? "Yay! You won the game!!!\nLet's play another one!":"Sorry, it seems that you lost the game...\nLet's play another one!";
+    if (playerPoints===5){
+        narratorText.innerText = "Yay! You won the game!!!\nLet's play another one!";
+        wonGameAudio.play();
+    } else if (computerPoints===5){
+        narratorText.innerText = "Sorry, it seems that you lost the game...\nLet's play another one!";
+        lostGameAudio.play();
+    }
 }
 
-function startNewGame(){    
-}
 
 function updateScore(){
     score.innerText = `SCORE: ${playerPoints} / ${computerPoints}`;
@@ -86,6 +88,7 @@ function updateScore(){
 
 const rockButton = document.querySelector("#rock-btn");
 rockButton.addEventListener("click", function(){
+    computerPlay();
     playerSelection = "rock";
     playRound();
     updateScore();
@@ -93,6 +96,7 @@ rockButton.addEventListener("click", function(){
 
 const paperButton = document.querySelector("#paper-btn");
 paperButton.addEventListener("click", function(){
+    computerPlay();
     playerSelection = "paper";
     playRound();
     updateScore();
@@ -100,6 +104,7 @@ paperButton.addEventListener("click", function(){
 
 const scissorsButton = document.querySelector("#scissors-btn");
 scissorsButton.addEventListener("click", function(){
+    computerPlay();
     playerSelection = "scissors";
     playRound();
     updateScore();
@@ -111,5 +116,56 @@ updateScore();
 const narratorText = document.querySelector(".narrator-text");
 
 
-// EXECUTE GAME
-//checkWinner();
+// ANIMATION
+
+const images = document.querySelectorAll(".fist-img");
+const leftFist = images.item(0);
+const rightFist = images.item(1);
+
+rockButton.addEventListener("click", function(){
+   revealLeft();
+   revealRight();
+})
+
+paperButton.addEventListener("click", function(){
+    revealLeft();
+    revealRight();
+ })
+
+ scissorsButton.addEventListener("click", function(){
+    revealLeft();
+    revealRight();
+ })
+
+
+function revealLeft(){
+    if (playerSelection === "rock"){
+        leftFist.src = "images/rock.png";
+    } else if (playerSelection === "paper"){
+        leftFist.src = "images/paper.png";
+    } else if (playerSelection === "scissors"){
+        leftFist.src = "images/scissors.png";
+    } else {
+        console.log("ERROR")
+    }
+}
+
+function revealRight(){
+    if (computerSelection === "rock"){
+        rightFist.src = "images/rock-inv.png";
+    } else if (computerSelection === "paper"){
+        rightFist.src = "images/paper-inv.png";
+    } else if (computerSelection === "scissors"){
+        rightFist.src = "images/scissors-inv.png";
+    } else {
+        console.log("ERROR")
+    }
+}
+
+// AUDIO
+
+const wonGameAudio = document.querySelector("#won-game");
+const lostGameAudio = document.querySelector("#lost-game");
+const wonRoundAudio = document.querySelector("#won-round");
+const lostRoundAudio = document.querySelector("#lost-round");
+const tieRoundAudio = document.querySelector("#tie-round");
